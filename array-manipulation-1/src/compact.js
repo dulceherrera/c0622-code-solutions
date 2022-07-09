4ea34fba6fa4d6c0fc725773320b8a80620e2902
Dulce Herrera <dulceherrera11@gmail.com>
2022-07-09 20:03:56 +0000

    Working on array-manipulation-1 exercise

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,8 @@
 /* exported compact */
+function compact(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (isNaN(array[i]) || array[i] === 0 || array[i] === null || array[i] === undefined || array[i] === false || array[i] === '' || array[i] === -Number) {
+      return array.splice(i, 1);
+    }
+  }
+}
--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -1,7 +1,8 @@
 /* exported reverse */
 function reverse(array) {
   var newArray = [];
-  for (var i = array.length - 1; i >= 0; i--) {
-    newArray.push(array[array[i]]);
+  for (var i = 0; i < array.length - 1; i++) {
+    newArray[i] = array[array.length - i - 1];
   }
+  return newArray;
 }
