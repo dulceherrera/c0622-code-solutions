211c767e866287ddc85a26a92985d0d70f5e8fba
Dulce Herrera <dulceherrera11@gmail.com>
2022-07-09 21:40:41 +0000

    Moving return statements from the line of closingcurly brace.

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -57,12 +57,14 @@ function getKeys(object) {
   var keys = [];
   for (var key in object) {
     keys.push(key);
-  } return keys;
+  }
+  return keys;
 }
 
 function getValues(object) {
   var values = [];
   for (var key in object) {
     values.push(object[key]);
-  } return values;
+  }
+  return values;
 }
