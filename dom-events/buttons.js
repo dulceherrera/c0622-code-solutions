0099e071139c0e30edea2fa3f37d5e649605b6e9
Dulce Herrera <dulceherrera11@gmail.com>
2022-07-11 21:52:14 +0000

    Corrected event target

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,7 +1,7 @@
 function handleClick(event) {
   console.log('button clicked');
   console.log('event:', event);
-  console.log('event.target:', $click);
+  console.log('event.target:', event.target);
 }
 var $click = document.querySelector('.click-button');
 $click.addEventListener('click', handleClick);
@@ -9,7 +9,7 @@ $click.addEventListener('click', handleClick);
 function handleMouseover(event) {
   console.log('button hovered');
   console.log('event', event);
-  console.log('event.target:', $hover);
+  console.log('event.target:', event.target);
 }
 var $hover = document.querySelector('.hover-button');
 $hover.addEventListener('mouseover', handleMouseover);
@@ -17,7 +17,7 @@ $hover.addEventListener('mouseover', handleMouseover);
 function handleDoubleClick(event) {
   console.log('button double-clicked');
   console.log('event', event);
-  console.log('event.target:', $doubleclick);
+  console.log('event.target:', event.target);
 }
 
 var $doubleclick = document.querySelector('.double-click-button');
