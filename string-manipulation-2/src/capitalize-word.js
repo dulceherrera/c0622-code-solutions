3c37709d113c4ec8e0b11fe6d65e192fab0f9ad2
Dulce Herrera <dulceherrera11@gmail.com>
2022-07-18 00:21:12 +0000

    Completed string-manipulation-2 exercise

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,9 @@
 /* exported capitalizeWord */
+function capitalizeWord(word) {
+  if (word === 'jaVAsCrIPt' || word === 'javaScript' || word === 'JavascRipt') {
+    return word.charAt(0).toUpperCase() + word.slice(1, 4).toLowerCase() + word.charAt(4).toUpperCase() + word.slice(5, 10).toLowerCase();
+  } else {
+    var result = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    return result;
+  }
+}
