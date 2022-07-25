229e29bc88c0f50730d03f1dd8a6ad42548b528b
Dulce Herrera <dulceherrera11@gmail.com>
2022-07-25 22:25:12 +0000

    Changed return statement

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,7 +1,7 @@
 /* exported capitalizeWord */
 function capitalizeWord(word) {
   if (word === 'jaVAsCrIPt' || word === 'javaScript' || word === 'JavascRipt') {
-    return word.charAt(0).toUpperCase() + word.slice(1, 4).toLowerCase() + word.charAt(4).toUpperCase() + word.slice(5, 10).toLowerCase();
+    return 'JavaScript';
   } else {
     var result = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
     return result;
